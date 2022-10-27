cda9c9fa9681c5d6b9c9d917c698beb3a0488144
Theo Ojochegbe <symplytheo@gmail.com>
2022-10-27 11:52:20 +0100

    feat: create auth-phone-number screen

--- src/navigations/stack.js
+++ src/navigations/stack.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppSplashScreen from '../screens/splash';
 import OnboardingScreen from '../screens/onboarding';
+import AuthPhoneNumberScreen from '../screens/auth/phone_number';
+import { Appbar, FAB } from 'react-native-paper';
+import { Colors } from '../styles/colors';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +14,25 @@ const StackNavigation = () => {
       <Stack.Group screenOptions={{ headerShown: false }}>
         <Stack.Screen name="welcome" component={AppSplashScreen} />
         <Stack.Screen name="onboarding" component={OnboardingScreen} />
+      </Stack.Group>
+
+      <Stack.Group
+        screenOptions={{
+          header: ({ navigation, back }) => (
+            <Appbar.Header style={{ backgroundColor: Colors.background }}>
+              {back ? (
+                <FAB
+                  customSize={48}
+                  mode="flat"
+                  icon="arrow-left"
+                  style={{ borderRadius: 8 + 0, marginLeft: 10 + 0 }}
+                  onPress={navigation.goBack}
+                />
+              ) : null}
+            </Appbar.Header>
+          ),
+        }}>
+        <Stack.Screen name="auth-phone" component={AuthPhoneNumberScreen} />
       </Stack.Group>
     </Stack.Navigator>
   );
