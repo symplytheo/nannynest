6c79e5ed68b17d5361c645f66a2084d835b2281d
Theo Ojochegbe <symplytheo@gmail.com>
2022-11-21 17:48:20 +0100

    chore: include icons

--- src/screens/ordering/livefeed.js
+++ src/screens/ordering/livefeed.js
@@ -7,8 +7,7 @@ import {
 } from 'react-native';
 import { styles } from './styles';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import AppButton from '../../components/appbutton';
-import { FAB } from 'react-native-paper';
+import { FAB, Text } from 'react-native-paper';
 import livefeed from '../../assets/img/livefeed.png';
 import { Colors } from '../../styles/colors';
 import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -20,21 +19,41 @@ const LiveFeedScreen = ({ navigation }) => {
     <SafeAreaView style={styles.wrapper}>
       <ImageBackground source={livefeed} style={styles.mapBg}>
         <View style={[styles.orderDate, { paddingBottom: insets.top }]}>
-          <TouchableOpacity
-            activeOpacity={0.7}
-            style={[styles.mb16, styles.arrow]}>
+          <TouchableOpacity activeOpacity={0.7} style={styles.arrow}>
             <MCIcon name="chevron-down" size={24} />
           </TouchableOpacity>
-          <View style={[styles.rowed, styles.mb16]}>
-            <AppButton mode="text">Chat</AppButton>
-            <AppButton
-              mode="text"
+          <View style={[styles.rowed, styles.p16]}>
+            <TouchableOpacity
+              activeOpacity={0.75}
+              style={styles.livefeedAction}
+              onPress={() => navigation.navigate('chat')}>
+              <MCIcon name="message-text" size={28} color={Colors.primary} />
+              <Text
+                variant="bodyMedium"
+                style={[styles.label, { color: Colors.primary }]}>
+                Chat
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              activeOpacity={0.75}
+              style={styles.livefeedAction}
               onPress={() => navigation.navigate('checklist')}>
-              Checklist
-            </AppButton>
+              <MCIcon
+                name="order-bool-ascending-variant"
+                size={28}
+                color={Colors.primary}
+              />
+              <Text
+                variant="bodyMedium"
+                style={[styles.label, { color: Colors.primary }]}>
+                Checklist
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
 
+        {/* Close Icon on Feed */}
         <View style={[styles.closeFabContainer, { top: insets.top }]}>
           <FAB
             customSize={48}
