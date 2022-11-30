51a9fe56ceeb2b1de98c15a83a2d55058ce0eda7
Theo Ojochegbe <symplytheo@gmail.com>
2022-11-30 15:24:54 +0100

    feat: create profile tab screen

---
+++ src/screens/profile/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import { Text } from 'react-native-paper';
+import MCIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import person from '../../assets/img/person.png';
+import { Colors } from '../../styles/colors';
+import styles from './styles';
+
+const SCREENS = [
+  { icon: 'cash-multiple', text: 'Payment', route: '' },
+  { icon: 'account', text: 'Profile', route: '' },
+  { icon: 'map-marker', text: 'Address book', route: '' },
+  { icon: 'cog', text: 'Settings', route: '' },
+  { icon: 'information', text: 'About', route: '' },
+  { icon: 'help-circle', text: 'Help', route: '' },
+];
+
+const ProfileTabScreen = () => {
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image source={person} style={[styles.dp, styles.mb8]} />
+        <Text variant="bodyLarge" style={styles.bold}>
+          Matthew Promise Theo
+        </Text>
+        <Text variant="bodyMedium" style={[{ color: Colors.grey }, styles.mb8]}>
+          +234 816 770 2086
+        </Text>
+        <View style={{ marginVertical: 8 + 0 }}>
+          {SCREENS.map(screen => (
+            <TouchableOpacity
+              key={screen.text}
+              activeOpacity={0.7}
+              style={styles.tabActionItem}>
+              <MCIcon
+                name={screen.icon}
+                style={{ marginRight: 16 + 0 }}
+                size={24}
+                color={Colors.primary}
+              />
+              <Text variant="bodyMedium">{screen.text}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default ProfileTabScreen;
---
+++ src/screens/profile/styles.js
@@ -0,0 +1,22 @@
+import { Platform, StyleSheet } from 'react-native';
+import { Colors } from '../../styles/colors';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  container: { padding: 16 },
+  dp: { height: 56, width: 56, borderRadius: 16 },
+  mb8: { marginBottom: 8 },
+  bold: { fontWeight: Platform.OS === 'ios' ? '700' : '600' },
+  tabActionItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 24,
+    borderTopWidth: 1,
+    borderColor: Colors.grey + '33',
+  },
+});
+
+export default styles;
