edab9742c1329168ce225d6e3984fbc31d823b8d
Theo Ojochegbe <symplytheo@gmail.com>
2023-02-09 15:08:05 +0100

    feat: add random data for nannies

---
+++ src/assets/js/data.js
@@ -0,0 +1,461 @@
+export const NANNIES = [
+  {
+    id: '63e4f23cd4aec44c28fbe3e9',
+    rating: 3.7,
+    name: 'Calderon Barr',
+    dateOfBirth: '1988-09-12',
+    gender: 'male',
+    about:
+      'Duis eu cillum ea Lorem velit non. Esse amet voluptate nisi nostrud veniam reprehenderit ut magna enim. Velit mollit velit qui esse enim do excepteur veniam nisi anim. Non ut anim eu ea sit consequat do deserunt excepteur dolor nostrud nostrud quis dolor.',
+    distanceKm: 3,
+    categories: ['child'],
+    languages: 'English',
+    nationality: 'Nepal',
+    yearsOfExperience: 3,
+    reviews: [
+      {
+        id: 0,
+        rating: 1.3,
+        comment:
+          'Ipsum mollit sint veniam et duis irure nostrud amet non culpa tempor voluptate ea exercitation. Sit ex aliqua laborum veniam cillum ex culpa amet.',
+      },
+      {
+        id: 1,
+        rating: 2.6,
+        comment:
+          'Eu magna fugiat labore fugiat labore aute. Velit esse sunt exercitation non excepteur deserunt sint veniam voluptate voluptate aliqua excepteur sit.',
+      },
+      {
+        id: 2,
+        rating: 2.4,
+        comment:
+          'Amet do adipisicing exercitation aliquip sunt esse mollit ad est ea dolore nisi. Excepteur sunt esse mollit velit magna non adipisicing est velit deserunt.',
+      },
+      {
+        id: 3,
+        rating: 2,
+        comment:
+          'Aliquip ea pariatur est qui exercitation irure exercitation nostrud aute esse ut excepteur occaecat mollit. Occaecat cillum elit est ea ut ipsum amet qui exercitation cupidatat ut esse nulla.',
+      },
+      {
+        id: 4,
+        rating: 1.2,
+        comment:
+          'Ea qui amet reprehenderit velit occaecat eiusmod excepteur nisi. Aliqua eu et pariatur Lorem ullamco officia ad officia aute ullamco eiusmod aliquip.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c39e5802186177cdd',
+    rating: 3,
+    name: 'Jeri Wolf',
+    dateOfBirth: '1999-07-07',
+    gender: 'female',
+    about:
+      'Consectetur incididunt cillum nisi cillum. Consectetur ex deserunt cupidatat officia sunt adipisicing labore nostrud ut ullamco aute dolore. Elit irure eu irure ullamco id cupidatat. Nisi mollit aliquip minim nostrud do eiusmod ea sunt laboris tempor qui aute aliquip.',
+    distanceKm: 4,
+    categories: ['child', 'toddler', 'elderly'],
+    languages: 'French',
+    nationality: 'Guam',
+    yearsOfExperience: 6,
+    reviews: [
+      {
+        id: 0,
+        rating: 2.6,
+        comment:
+          'Et anim in excepteur quis est eu pariatur. Velit velit incididunt amet mollit Lorem ullamco mollit qui eiusmod elit amet commodo.',
+      },
+      {
+        id: 1,
+        rating: 1,
+        comment:
+          'Deserunt cupidatat est consectetur ea nulla anim et. Cillum sint irure reprehenderit exercitation nisi amet qui id cupidatat excepteur consequat occaecat nulla.',
+      },
+      {
+        id: 2,
+        rating: 1.6,
+        comment:
+          'Aliquip anim cupidatat adipisicing dolor voluptate. Qui elit eiusmod veniam quis non sint mollit dolore magna laboris.',
+      },
+      {
+        id: 3,
+        rating: 1.4,
+        comment:
+          'In nisi nisi proident pariatur nostrud Lorem anim nisi aute cupidatat sunt et ea. Magna commodo elit mollit excepteur irure sit dolor sint consectetur consequat fugiat adipisicing.',
+      },
+      {
+        id: 4,
+        rating: 1.9,
+        comment:
+          'Non in anim dolore do non exercitation id do exercitation velit proident laborum voluptate voluptate. Elit tempor fugiat reprehenderit ex ea velit non id dolor elit.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23cd20fd0abb6673235',
+    rating: 4.9,
+    name: 'Britt Lawrence',
+    dateOfBirth: '1985-10-03',
+    gender: 'male',
+    about:
+      'Amet commodo fugiat veniam dolor dolor anim id occaecat ullamco excepteur occaecat voluptate adipisicing ad. Aliqua quis mollit elit laborum anim et commodo non culpa id id excepteur. Exercitation incididunt consequat anim deserunt consequat laborum proident laboris sit non enim non. Incididunt dolor laborum exercitation eu quis ad eiusmod nisi.',
+    distanceKm: 6,
+    categories: ['child'],
+    languages: 'English',
+    nationality: 'Papua New Guinea',
+    yearsOfExperience: 6,
+    reviews: [
+      {
+        id: 0,
+        rating: 4.8,
+        comment:
+          'Labore excepteur velit officia duis adipisicing nisi qui Lorem ad. Est non minim ad tempor deserunt qui.',
+      },
+      {
+        id: 1,
+        rating: 1.8,
+        comment:
+          'Veniam adipisicing cillum cillum mollit consectetur duis exercitation minim. Dolor enim elit aliqua occaecat fugiat quis tempor cupidatat deserunt.',
+      },
+      {
+        id: 2,
+        rating: 4.9,
+        comment: 'Do dolore excepteur Lorem pariatur nisi. Cupidatat ex est ea aute.',
+      },
+      {
+        id: 3,
+        rating: 1.2,
+        comment:
+          'Do ullamco mollit aliquip commodo exercitation dolore. Laborum aute nisi reprehenderit et deserunt sunt irure minim excepteur labore enim eiusmod exercitation.',
+      },
+      {
+        id: 4,
+        rating: 3.2,
+        comment:
+          'Cillum officia qui sit id qui elit sit irure labore elit. Nulla occaecat commodo eu non consectetur reprehenderit consequat dolore ad quis.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c271d51efa413828a',
+    rating: 3.9,
+    name: 'Valeria Lucas',
+    dateOfBirth: '1995-03-27',
+    gender: 'female',
+    about:
+      'Lorem velit nulla incididunt incididunt. Aliqua officia id esse irure aliqua ad proident. Lorem ad minim aliqua irure minim laboris et id eu laboris do culpa. Consequat aute dolor eiusmod aliquip velit sit.',
+    distanceKm: 1,
+    categories: ['child', 'toddler', 'elderly'],
+    languages: 'Portugese',
+    nationality: 'Sweden',
+    yearsOfExperience: 7,
+    reviews: [
+      {
+        id: 0,
+        rating: 1.7,
+        comment:
+          'Tempor id velit sunt nulla amet dolore anim commodo mollit do dolor. Consectetur reprehenderit consequat tempor do aute esse pariatur ad anim incididunt ut.',
+      },
+      {
+        id: 1,
+        rating: 4.6,
+        comment:
+          'Labore tempor veniam laboris elit Lorem incididunt labore proident irure. Anim nisi commodo incididunt proident deserunt aliqua reprehenderit deserunt id in adipisicing laboris do.',
+      },
+      {
+        id: 2,
+        rating: 2.4,
+        comment:
+          'Quis mollit adipisicing elit sint Lorem sint ea fugiat mollit officia ipsum voluptate non. Cupidatat et veniam commodo veniam aliquip cillum do excepteur elit excepteur adipisicing enim veniam.',
+      },
+      {
+        id: 3,
+        rating: 2.9,
+        comment:
+          'Consequat magna exercitation tempor amet proident proident aliquip exercitation nisi minim mollit nisi eu elit. Nostrud et ullamco exercitation elit adipisicing sunt dolore.',
+      },
+      {
+        id: 4,
+        rating: 4,
+        comment:
+          'Dolor dolor proident Lorem ipsum voluptate ut sunt occaecat pariatur sit in amet fugiat deserunt. Magna consequat elit qui dolor ullamco ipsum.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c2709efb777b17cfe',
+    rating: 4.2,
+    name: 'Clarissa Reilly',
+    dateOfBirth: '2000-10-20',
+    gender: 'female',
+    about:
+      'Reprehenderit dolor exercitation sit quis fugiat non fugiat ea cillum labore esse ut ipsum. Officia irure culpa do sit deserunt excepteur nulla esse minim adipisicing. Excepteur deserunt laborum non reprehenderit qui laboris ea. Tempor commodo anim nulla deserunt et sint duis deserunt ullamco sint do enim.',
+    distanceKm: 3,
+    categories: ['child'],
+    languages: 'Portugese',
+    nationality: 'Nigeria',
+    yearsOfExperience: 7,
+    reviews: [
+      {
+        id: 0,
+        rating: 1.4,
+        comment:
+          'Reprehenderit consequat ullamco anim eiusmod aliquip consectetur labore id quis amet ut laboris aliqua. Dolore officia ex mollit commodo laboris.',
+      },
+      {
+        id: 1,
+        rating: 1.5,
+        comment:
+          'Minim sunt cupidatat aliquip est Lorem esse. Veniam cillum minim commodo aliquip sit incididunt labore tempor eu consectetur tempor adipisicing dolor magna.',
+      },
+      {
+        id: 2,
+        rating: 2.3,
+        comment:
+          'Eu incididunt et excepteur ut cillum in qui laborum adipisicing. Pariatur consectetur proident anim voluptate.',
+      },
+      {
+        id: 3,
+        rating: 3.7,
+        comment:
+          'Occaecat et elit laborum esse sint qui pariatur commodo occaecat qui ipsum aute cupidatat. Veniam ut deserunt cupidatat id nulla tempor elit occaecat.',
+      },
+      {
+        id: 4,
+        rating: 2.4,
+        comment:
+          'Anim nisi tempor sit nisi dolore enim exercitation non reprehenderit. Deserunt laborum dolore laborum et consequat in.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c39f776880249ac79',
+    rating: 3.6,
+    name: 'Kemp Kent',
+    dateOfBirth: '1991-07-25',
+    gender: 'male',
+    about:
+      'Id esse tempor duis do ea consectetur consectetur ullamco. Commodo anim dolore dolore cupidatat ipsum dolore ea laborum occaecat ea ullamco est adipisicing eu. Proident eu irure culpa culpa esse. Culpa reprehenderit commodo eiusmod nostrud enim magna tempor aliqua qui sit veniam consectetur.',
+    distanceKm: 8,
+    categories: ['child'],
+    languages: 'French',
+    nationality: 'France, Metropolitan',
+    yearsOfExperience: 1,
+    reviews: [
+      {
+        id: 0,
+        rating: 4.8,
+        comment:
+          'Elit aliqua veniam occaecat cillum sit veniam in consequat. Elit ex proident ex minim quis minim officia occaecat esse reprehenderit.',
+      },
+      {
+        id: 1,
+        rating: 2.2,
+        comment:
+          'Et aute ea exercitation aliqua mollit. Voluptate irure culpa Lorem aliqua commodo laboris eu eiusmod incididunt anim id culpa velit cupidatat.',
+      },
+      {
+        id: 2,
+        rating: 5,
+        comment:
+          'Occaecat et incididunt esse excepteur cupidatat voluptate fugiat proident dolore deserunt fugiat. Incididunt adipisicing consequat commodo tempor anim minim sit labore esse do ex.',
+      },
+      {
+        id: 3,
+        rating: 4.8,
+        comment:
+          'Ut proident Lorem minim non adipisicing magna ad pariatur ullamco qui pariatur ex velit. Culpa non ad ipsum excepteur tempor excepteur irure.',
+      },
+      {
+        id: 4,
+        rating: 2.3,
+        comment:
+          'Est commodo aute exercitation pariatur sit et pariatur. Aliquip sit velit irure minim anim irure consequat minim veniam laborum Lorem qui.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23cbc509594ed05f1ff',
+    rating: 4.6,
+    name: 'Caldwell Beck',
+    dateOfBirth: '1991-05-27',
+    gender: 'male',
+    about:
+      'Pariatur consectetur mollit non sint proident elit nisi duis cillum ut. Sunt fugiat irure sunt consequat exercitation. Laboris nulla deserunt culpa deserunt laboris ullamco non exercitation. Incididunt ea cillum tempor dolore aute sint nulla do minim exercitation fugiat commodo.',
+    distanceKm: 4,
+    categories: ['child', 'toddler'],
+    languages: 'Afrikan',
+    nationality: 'Albania',
+    yearsOfExperience: 1,
+    reviews: [
+      {
+        id: 0,
+        rating: 3.5,
+        comment:
+          'Incididunt consequat ex laborum deserunt pariatur. Ad veniam ut enim amet id esse adipisicing velit nulla pariatur mollit esse.',
+      },
+      {
+        id: 1,
+        rating: 4.6,
+        comment:
+          'Aliqua eu qui sint amet et aliqua ullamco nostrud laboris non consectetur. Elit in laboris reprehenderit cillum proident ex Lorem commodo tempor.',
+      },
+      {
+        id: 2,
+        rating: 1.5,
+        comment:
+          'Esse nisi et nostrud veniam anim consequat dolor cillum pariatur. Ullamco aliquip id ad adipisicing consectetur anim veniam nostrud.',
+      },
+      {
+        id: 3,
+        rating: 1.2,
+        comment:
+          'Velit quis occaecat exercitation ut consectetur quis sit anim est mollit tempor nostrud. Laborum deserunt eiusmod esse dolore sit aliquip eu exercitation deserunt proident nulla laborum duis.',
+      },
+      {
+        id: 4,
+        rating: 4.5,
+        comment:
+          'Nisi officia esse duis elit aute enim nulla et voluptate id dolore. Officia nisi adipisicing dolor ad.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c093f09ffc73fda0d',
+    rating: 4.5,
+    name: 'Tina Clayton',
+    dateOfBirth: '1985-01-26',
+    gender: 'female',
+    about:
+      'Mollit dolor cillum quis laborum cupidatat non deserunt eiusmod nisi ea. Non dolor minim laboris cupidatat. Ex amet magna officia nisi aute amet non duis est deserunt elit tempor cupidatat. Magna id eiusmod tempor tempor pariatur.',
+    distanceKm: 8,
+    categories: ['child', 'toddler', 'elderly'],
+    languages: 'Portugese',
+    nationality: 'Indonesia',
+    yearsOfExperience: 4,
+    reviews: [
+      {
+        id: 0,
+        rating: 4.8,
+        comment:
+          'Culpa consequat officia sunt cupidatat dolor. Incididunt exercitation cillum enim Lorem quis elit excepteur eiusmod excepteur est sunt qui reprehenderit magna.',
+      },
+      {
+        id: 1,
+        rating: 3.6,
+        comment:
+          'Deserunt culpa sunt nostrud dolore cillum et ex cillum. Cupidatat ullamco ullamco aute in tempor ullamco culpa.',
+      },
+      {
+        id: 2,
+        rating: 1.3,
+        comment:
+          'Officia esse dolor ex occaecat id elit cupidatat. Exercitation consectetur sunt enim fugiat nostrud cillum in officia.',
+      },
+      {
+        id: 3,
+        rating: 4.3,
+        comment:
+          'Sint eiusmod et elit consectetur nisi deserunt dolore sint do. Esse amet do velit et aliqua excepteur qui dolor dolor laborum fugiat.',
+      },
+      {
+        id: 4,
+        rating: 1.3,
+        comment:
+          'Deserunt mollit et Lorem veniam mollit anim. Id non elit pariatur consectetur non.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c5562df0b3c4878b0',
+    rating: 3.8,
+    name: 'Laurel Gillespie',
+    dateOfBirth: '1986-11-12',
+    gender: 'female',
+    about:
+      'Duis culpa cillum anim laborum ex est aute do ad. Aliqua anim ad cupidatat culpa nostrud reprehenderit irure eiusmod nulla culpa exercitation esse. Non ullamco cupidatat id cupidatat aliquip reprehenderit aliqua aliqua amet. Ea et et sit culpa aute nostrud anim aliqua quis.',
+    distanceKm: 7,
+    categories: ['child', 'toddler', 'elderly'],
+    languages: 'Portugese',
+    nationality: 'Antigua and Barbuda',
+    yearsOfExperience: 7,
+    reviews: [
+      {
+        id: 0,
+        rating: 3.8,
+        comment:
+          'Anim magna tempor aliqua tempor irure aliqua pariatur reprehenderit in non occaecat veniam. Nostrud enim laboris mollit cillum nostrud ipsum tempor proident officia dolore culpa.',
+      },
+      {
+        id: 1,
+        rating: 3.2,
+        comment:
+          'Duis aute dolore Lorem qui sint do labore velit officia reprehenderit laborum veniam. Commodo amet ullamco reprehenderit proident et commodo aliquip sunt elit esse.',
+      },
+      {
+        id: 2,
+        rating: 1.3,
+        comment:
+          'Ut ad deserunt sit minim consequat et duis fugiat duis laborum tempor culpa dolore. Eiusmod consequat duis dolore magna sint tempor fugiat aliqua consectetur.',
+      },
+      {
+        id: 3,
+        rating: 2.9,
+        comment:
+          'Ipsum in cillum Lorem irure nulla ipsum laborum non. Sunt enim laboris elit ex cillum pariatur aliqua ea ad consequat non incididunt.',
+      },
+      {
+        id: 4,
+        rating: 3.3,
+        comment:
+          'Ad non enim ullamco nostrud voluptate minim aliqua id non. Aliqua amet consectetur commodo eiusmod ullamco do fugiat nisi cillum officia et eiusmod voluptate commodo.',
+      },
+    ],
+  },
+  {
+    id: '63e4f23c9602823fdfb63897',
+    rating: 4.4,
+    name: 'Rush Lang',
+    dateOfBirth: '2003-10-09',
+    gender: 'male',
+    about:
+      'Qui mollit reprehenderit quis ad amet ex anim laborum. Irure fugiat sunt in officia voluptate sint. Et proident incididunt ut commodo est ullamco cillum dolor officia. Duis excepteur ex sit reprehenderit cillum voluptate exercitation mollit fugiat aute incididunt sit.',
+    distanceKm: 8,
+    categories: ['child', 'toddler', 'elderly'],
+    languages: 'Portugese',
+    nationality: 'Iran',
+    yearsOfExperience: 6,
+    reviews: [
+      {
+        id: 0,
+        rating: 2.4,
+        comment:
+          'Aliquip nisi anim minim laborum est incididunt aliqua dolor culpa irure. Laborum deserunt reprehenderit enim ad magna pariatur adipisicing ullamco ad est cupidatat id fugiat.',
+      },
+      {
+        id: 1,
+        rating: 1.4,
+        comment:
+          'Laboris veniam excepteur magna qui nulla voluptate reprehenderit ullamco cupidatat do proident commodo tempor ullamco. Consequat ea pariatur duis consectetur.',
+      },
+      {
+        id: 2,
+        rating: 4.6,
+        comment:
+          'Aute fugiat dolore veniam fugiat amet proident occaecat enim minim dolor Lorem adipisicing ullamco in. Sunt eiusmod mollit duis officia minim ea reprehenderit ut mollit est incididunt.',
+      },
+      {
+        id: 3,
+        rating: 3,
+        comment:
+          'Enim magna fugiat fugiat pariatur sunt eu. Ipsum ad cillum in minim id eu duis excepteur irure adipisicing dolore deserunt.',
+      },
+      {
+        id: 4,
+        rating: 4.2,
+        comment:
+          'Aute ipsum elit sit qui qui amet cupidatat dolor anim. Culpa amet quis eu ipsum eu labore cupidatat elit occaecat in laboris in.',
+      },
+    ],
+  },
+];
